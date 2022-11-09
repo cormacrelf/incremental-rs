df94cd85017e73a56d8af352cbb85592f01661e0
Cormac Relf <web@cormacrelf.net>
2022-11-09 22:28:53 +1100

    Add two benchmarks from the original OCaml
    Linear trivial/50 is running at 17ns/node.
    The rest closer to 30ns/node, bind 90ns/node.
    Map has comparable perf to the OCaml.

---
+++ benches/linear.rs
@@ -0,0 +1,140 @@
+use std::time::Instant;
+
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+
+use incremental::{IncrState, Observer, Incr, Var};
+
+#[derive(Clone, Copy)]
+enum SequenceKind {
+    Trivial,
+    TrivialBind,
+    Recombine,
+    Wide,
+}
+
+fn sequence(node: Incr<u64>, kind: SequenceKind, size: u64) -> Incr<u64> {
+    match kind {
+        SequenceKind::Trivial => {
+            (0..size).into_iter().fold(node, |node, _| {
+                node.map(|val| val + 1)
+            })
+        }
+        SequenceKind::TrivialBind => {
+            (0..size).into_iter().fold(node, |node, _| {
+                node.binds(|incr, &val| incr.constant(val + 1))
+            })
+        }
+        SequenceKind::Recombine => {
+            (0..size).into_iter().fold(node, |node, _| {
+                let a = node.map(|x| x + 1);
+                let b = node.map(|x| x + 1);
+                (a % b).map(|a, b| a + b)
+            })
+        }
+        SequenceKind::Wide => {
+            let double = |list: Vec<Incr<u64>>| -> Vec<Incr<_>> {
+                list.into_iter()
+                    .map(|i| {
+                        let a = i.map(|x| x + 1);
+                        let b = i.map(|x| x + 1);
+                        [a, b]
+                    })
+                    .flatten()
+                    .collect()
+            };
+            let spread = (0..size).into_iter().fold(vec![node], |acc, _| double(acc));
+            reduce_balanced(&spread, |a, b| a.map2(b, |a, b| a + b))
+                .unwrap()
+        }
+    }
+}
+
+fn reduce_balanced<T: Clone>(slice: &[T], mut f: impl FnMut(&T, &T) -> T) -> Option<T> {
+    fn reduce_balanced_inner<T: Clone>(slice: &[T], f: &mut impl FnMut(&T, &T) -> T) -> Option<T> {
+        let size = slice.len();
+        if size == 0 { return None }
+        if size == 1 { return slice.first().cloned() }
+        let mid = size / 2;
+        let left = reduce_balanced_inner(&slice[..mid], f).unwrap();
+        let right = reduce_balanced_inner(&slice[mid..], f).unwrap();
+        Some(f(&left, &right))
+    }
+    reduce_balanced_inner(slice, &mut f)
+}
+
+#[test]
+fn test_reduce_balanced() {
+    let slice: &[i32] = &[];
+    assert_eq!(reduce_balanced(slice, |a, b| a + b), None);
+    let slice = &[1];
+    assert_eq!(reduce_balanced(slice, |a, b| a + b), Some(1));
+    let slice = &[1, 2];
+    assert_eq!(reduce_balanced(slice, |a, b| a + b), Some(3));
+    let slice = &[1, 2, 3, 4];
+    assert_eq!(reduce_balanced(slice, |a, b| a + b), Some(10));
+    let slice = &[1, 2, 3, 4, 5];
+    assert_eq!(reduce_balanced(slice, |a, b| a + b), Some(15));
+}
+
+fn setup(kind: SequenceKind, size: u64) -> (Var<u64>, IncrState, Observer<u64>, u32) {
+    let incr = IncrState::new_with_height(1200);
+    let first_num = incr.var(1u64);
+    let o = first_num.pipe2(sequence, kind, size).observe();
+    incr.stabilise();
+    first_num.set(0);
+    let stats = incr.stats();
+    incr.stabilise();
+    let recomputed = incr.stats().diff(stats).recomputed as u32;
+    (first_num, incr, o, recomputed)
+}
+
+impl From<SequenceKind> for String {
+    fn from(value: SequenceKind) -> Self {
+        match value {
+            SequenceKind::Wide => "wide",
+            SequenceKind::Trivial => "trivial",
+            SequenceKind::TrivialBind => "trivial-bind",
+            SequenceKind::Recombine => "recombine",
+        }
+        .into()
+    }
+}
+
+fn bench_node(c: &mut Criterion, kind: SequenceKind, size: u64) {
+    c.bench_with_input(BenchmarkId::new(kind, size), &size, |b, &size| {
+        let (var, incr, obs, _recomputed) = setup(kind, size);
+        b.iter_custom(|iters| {
+            let start = Instant::now();
+            for _ in 0..iters {
+                var.update(|x| *x += 1);
+                incr.stabilise();
+                drop(obs.expect_value());
+            }
+            start.elapsed()
+        })
+    });
+}
+
+fn bench_stabilise(c: &mut Criterion, kind: SequenceKind, size: u64) {
+    c.bench_with_input(BenchmarkId::new("stabilise", size), &size, |b, &size| {
+        let (_var, incr, obs, _recomputed) = setup(kind, size);
+        b.iter(|| {
+            incr.stabilise();
+            drop(obs.expect_value());
+        })
+    });
+}
+
+#[tracing::instrument(skip_all)]
+fn all(c: &mut Criterion) {
+    tracing_subscriber::fmt().init();
+    bench_node(c, SequenceKind::Recombine, 50);
+    bench_node(c, SequenceKind::Trivial, 50);
+    bench_node(c, SequenceKind::TrivialBind, 50);
+    bench_node(c, SequenceKind::Wide, 5);
+    bench_node(c, SequenceKind::Wide, 10);
+    bench_stabilise(c, SequenceKind::Recombine, 50);
+}
+
+criterion_group!(benches, all);
+criterion_main!(benches);
---
+++ benches/map.rs
@@ -0,0 +1,206 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use im_rc::{OrdMap, ordmap::Entry};
+use incremental::{Incr, Value, IncrState};
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum Dir {
+    Buy,
+    Sell,
+}
+
+type Symbol = u32;
+type Oid = u32;
+
+
+#[derive(Clone, PartialEq)]
+struct Order {
+    // OCaml strings are GC'd.
+    id: Oid,
+    price: f32,
+    size: u32,
+    sym: Symbol,
+    dir: Dir,
+}
+impl std::fmt::Debug for Order {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Order")
+            .field(&self.id)
+            .field(&self.dir)
+            .field(&self.price)
+            .field(&self.size)
+            .field(&self.sym)
+            .finish()
+    }
+}
+
+fn index_by<KInner: Value + Ord, KOuter: Value + Ord, V: Value>(
+    map: Incr<OrdMap<KInner, V>>,
+    get_outer_index: impl Fn(&V) -> KOuter + Clone + 'static,
+) -> Incr<OrdMap<KOuter, OrdMap<KInner, V>>> {
+    let get_outer_index_ = get_outer_index.clone();
+    let add = move |mut acc: OrdMap<KOuter, OrdMap<KInner, V>>, key_inner: &KInner, value: &V| {
+        let index = get_outer_index_(value);
+        acc.entry(index)
+            .or_insert_with(|| OrdMap::new())
+            .insert(key_inner.clone(), value.clone());
+        acc
+    };
+    let remove =
+        move |mut acc: OrdMap<KOuter, OrdMap<KInner, V>>, key_inner: &KInner, value: &V| {
+            let index = get_outer_index(value);
+            match acc.entry(index) {
+                Entry::Vacant(_) => panic!(),
+                Entry::Occupied(mut o) => {
+                    let map = o.get_mut();
+                    map.remove(key_inner);
+                    if map.is_empty() {
+                        o.remove();
+                    }
+                }
+            }
+            acc
+        };
+    let indexed = map.incr_unordered_fold(OrdMap::new(), add, remove, false);
+    #[cfg(debug_assertions)]
+    indexed.set_graphviz_user_data("index_by");
+    indexed
+}
+
+#[test]
+fn test_index_by() {
+    let incr = IncrState::new();
+    let var = incr.var(OrdMap::<i32, String>::default());
+    let o = index_by(var.watch(), |x| x.to_uppercase()).observe();
+    let insert = |k: i32, val: &str| {
+        var.update(|map| { map.insert(k, val.to_string()); });
+        incr.stabilise();
+        o.expect_value()
+    };
+    use im_rc::ordmap;
+    assert_eq!(insert(1, "bar"), ordmap! {
+        "BAR".to_string() => ordmap! {
+            1i32 => "bar".to_string()
+        }
+    });
+    assert_eq!(insert(1, "foo"), ordmap! {
+        "FOO".to_string() => ordmap! {
+            1i32 => "foo".to_string()
+        }
+    });
+    assert_eq!(insert(2, "foo"), ordmap! {
+        "FOO".to_string() => ordmap! {
+            1i32 => "foo".to_string(),
+            2i32 => "foo".to_string()
+        }
+    });
+    assert_eq!(insert(3, "bar"), ordmap! {
+        "BAR".to_string() => ordmap! {
+            3i32 => "bar".to_string()
+        },
+        "FOO".to_string() => ordmap! {
+            1i32 => "foo".to_string(),
+            2i32 => "foo".to_string()
+        }
+    });
+    assert_eq!(insert(2, "bar"), ordmap! {
+        "BAR".to_string() => ordmap! {
+            2i32 => "bar".to_string(),
+            3i32 => "bar".to_string()
+        },
+        "FOO".to_string() => ordmap! {
+            1i32 => "foo".to_string()
+        }
+    });
+}
+
+fn shares(orders: Incr<OrdMap<Oid, Order>>) -> Incr<u32> {
+    orders.incr_unordered_fold(
+        0,
+        |acc, _k, x| acc + x.size,
+        |acc, _k, x| acc - x.size,
+        false,
+    )
+}
+
+fn shares_per_symbol(orders: Incr<OrdMap<Oid, Order>>) -> Incr<OrdMap<Symbol, u32>> {
+    orders
+        .pipe1(index_by, |x| x.sym.clone())
+        .incr_mapi_(|_k, v| shares(v.clone()), None)
+}
+
+fn shares_per_symbol_flat(orders: Incr<OrdMap<Oid, Order>>) -> Incr<OrdMap<Symbol, u32>> {
+    fn update_sym_map(op: fn(u32, u32) -> u32) -> impl FnMut(OrdMap<Symbol, u32>, &Symbol, &Order) -> OrdMap<Symbol, u32> {
+        move |mut acc, _k, o| {
+            match acc.entry(_k.clone()) {
+                Entry::Vacant(e) => { e.insert(o.size); }
+                Entry::Occupied(mut e) => { e.insert(op(*e.get(), o.size)); }
+            }
+            acc
+        }
+    }
+    orders
+        .incr_unordered_fold(
+            OrdMap::new(),
+            update_sym_map(|a, b| a + b),
+            update_sym_map(|a, b| a - b),
+            false,
+        )
+}
+
+fn random_order(rng: &mut impl rand::Rng) -> Order {
+    let num_symbols = 100;
+    let sym = rng.gen_range(0..num_symbols);
+    let size = rng.gen_range(0..10_000);
+    let price = rng.gen_range(0..10_000) as f32 / 100.;
+    let dir = if rng.gen_ratio(1, 2) { Dir::Buy } else { Dir::Sell };
+    let id = rng.gen_range(0..u32::MAX);
+    Order {
+        id, dir, price, size, sym
+    }
+}
+
+fn random_orders(rng: &mut impl rand::Rng, n: u32) -> OrdMap<Oid, Order> {
+    (0..n).into_iter().fold(OrdMap::new(), |mut acc, _| {
+        let o = random_order(rng);
+        acc.insert(o.id.clone(), o);
+        acc
+    })
+}
+
+fn setup(n: u32, sps_fn: fn(Incr<OrdMap<Oid, Order>>) -> Incr<OrdMap<Symbol, u32>>) -> impl FnMut() {
+    tracing::info!("setup called");
+    let mut rng = rand::thread_rng();
+    let init_orders = random_orders(&mut rng, n);
+    let incr = IncrState::new();
+    let var = incr.var(init_orders.clone());
+    let shares = var.pipe(sps_fn).observe();
+    incr.stabilise();
+    if n < 100 {
+        shares.save_dot_to_file(&format!("shares-{}.dot", n));
+    }
+    move || {
+        let random = random_order(&mut rng);
+        var.set(init_orders.update(random.id.clone(), random));
+        incr.stabilise();
+        drop(shares.expect_value());
+    }
+}
+
+#[tracing::instrument(skip_all)]
+fn bench_update(c: &mut Criterion) {
+    tracing_subscriber::fmt().init();
+
+    let size = 1_000_000;
+    c.bench_with_input(BenchmarkId::new("nested", size), &size, |b, &size| {
+        let iter_fn = setup(size, shares_per_symbol);
+        b.iter(iter_fn)
+    });
+    c.bench_with_input(BenchmarkId::new("flat", size), &size, |b, &size| {
+        let iter_fn = setup(size, shares_per_symbol_flat);
+        b.iter(iter_fn)
+    });
+}
+
+criterion_group!(benches, bench_update);
+criterion_main!(benches);
