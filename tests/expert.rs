3180d1e19186696b0efaf473483796adf90fcc75
Cormac Relf <web@cormacrelf.net>
2023-05-14 13:07:08 +1000

    map345_expert test

--- tests/expert.rs
+++ tests/expert.rs
@@ -2,7 +2,7 @@ use std::{cell::RefCell, rc::Rc};
 
 use incremental::{expert::*, Incr, IncrState, Value};
 
-fn join<T: Value>(incr: Incr<Incr<T>>) -> Incr<T> {
+fn join<T: Value>(incr: &Incr<Incr<T>>) -> Incr<T> {
     let prev_rhs: Rc<RefCell<Option<Dependency<T>>>> = Rc::new(None.into());
     let state = incr.state();
     let join = Node::<T, T>::new(&state, {
@@ -10,6 +10,7 @@ fn join<T: Value>(incr: Incr<Incr<T>>) -> Incr<T> {
         move || prev_rhs_.borrow().clone().unwrap().value_cloned()
     });
     let join_ = join.weak();
+    // TODO: should not need to create a map node for this.
     let lhs_change = incr.map(move |rhs| {
         let dep = join_.add_dependency(rhs);
         let mut prev_rhs_ = prev_rhs.borrow_mut();
@@ -27,7 +28,7 @@ fn test_join() {
     let incr = IncrState::new();
     let inner = incr.var(10i32);
     let outer = incr.var(inner.watch());
-    let joined = join(outer.watch());
+    let joined = join(&outer);
     let o = joined.observe();
     incr.stabilise();
     assert_eq!(o.try_get_value(), Ok(10));
@@ -59,4 +60,21 @@ fn bind<T: Value, R: Value>(incr: Incr<T>, mut f: impl FnMut(&T) -> Incr<R> + 's
     });
     join.add_dependency_unit(&lhs_change);
     join.watch()
+}
+
+#[test]
+fn map345_expert() {
+    let incr = IncrState::new();
+    let i1 = incr.var(3);
+    let i2 = incr.var(5);
+    let i3 = incr.var(7);
+    let triple = i1.map3(&i2, &i3, |a, b, c| (a * b, *c));
+
+    // now have an expert node add a dependency on triple
+    let outer = incr.var(triple.clone());
+    let joined = join(&outer);
+
+    let j = joined.observe();
+    incr.stabilise();
+    assert_eq!(j.value(), (15, 7));
 }
