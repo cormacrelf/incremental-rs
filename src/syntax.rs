14a1f23dc9c5b34f90354bc587a860b76e2d7566
Cormac Relf <web@cormacrelf.net>
2023-11-10 15:31:20 +1100

    move some MapBuilders into a macro

--- src/syntax.rs
+++ src/syntax.rs
@@ -7,30 +7,59 @@ use super::Value;
 /// Produced by the syntax `i1 % i2` for two `Incr`s.
 pub struct MapBuilder2<I1, I2>(Incr<I1>, Incr<I2>);
 /// Produced by the syntax `i1 % i2 % i3` for three `Incr`s.
-pub struct MapBuilder3<I1, I2, I3>(Incr<I1>, Incr<I2>, Incr<I3>);
 impl<I1, I2> Rem<Incr<I2>> for Incr<I1> {
     type Output = MapBuilder2<I1, I2>;
     fn rem(self, rhs: Incr<I2>) -> Self::Output {
         MapBuilder2(self, rhs)
     }
 }
+
+macro_rules! map_builder {
+    (@inner $n:ident<$($upto_i:ident),+ > => $n_plus_1:ident<.., $i_plus_1:ident>) => {
+        pub struct $n_plus_1<$($upto_i,)+ $i_plus_1>($(Incr<$upto_i>,)+ Incr<$i_plus_1>);
+
+        impl<$($upto_i,)+ $i_plus_1> Rem<Incr<$i_plus_1>> for $n<$($upto_i,)+> {
+            type Output = $n_plus_1<$($upto_i,)+ $i_plus_1>;
+            fn rem(self, rhs: Incr<$i_plus_1>) -> Self::Output {
+                $n_plus_1(self.0, self.1, rhs)
+            }
+        }
+        impl<$($upto_i,)+ $i_plus_1> Rem<&Incr<$i_plus_1>> for $n<$($upto_i,)+> {
+            type Output = $n_plus_1<$($upto_i,)+ $i_plus_1>;
+            fn rem(self, rhs: &Incr<$i_plus_1>) -> Self::Output {
+                $n_plus_1(self.0, self.1, rhs.clone())
+            }
+        }
+    };
+
+    {
+        [
+            $n:ident<$($upto_i:ident),+>,
+        ]
+    } => {
+    };
+    {
+        [
+            $n:ident<$($upto_i:ident),+>,
+            $n_plus_1:ident<.., $i_plus_1:ident>,
+            $($rest:tt)*
+        ]
+    } => {
+        map_builder!(@inner $n<$($upto_i),+> => $n_plus_1<.., $i_plus_1>);
+        map_builder!([$n_plus_1<$($upto_i,)+ $i_plus_1>, $($rest)*]);
+    };
+}
+
+map_builder!([
+     MapBuilder2<I1, I2>,
+     MapBuilder3<.., I3>,
+     // MapBuilder4<.., I4>,
+]);
+
 impl<I1, I2> Rem<&Incr<I2>> for &Incr<I1> {
     type Output = MapBuilder2<I1, I2>;
     fn rem(self, rhs: &Incr<I2>) -> Self::Output {
         MapBuilder2(self.clone(), rhs.clone())
-    }
-}
-
-impl<I1, I2, I3> Rem<Incr<I3>> for MapBuilder2<I1, I2> {
-    type Output = MapBuilder3<I1, I2, I3>;
-    fn rem(self, rhs: Incr<I3>) -> Self::Output {
-        MapBuilder3(self.0, self.1, rhs)
-    }
-}
-impl<I1, I2, I3> Rem<&Incr<I3>> for MapBuilder2<I1, I2> {
-    type Output = MapBuilder3<I1, I2, I3>;
-    fn rem(self, rhs: &Incr<I3>) -> Self::Output {
-        MapBuilder3(self.0, self.1, rhs.clone())
     }
 }
 
